0b4c47300b81f5ad4b226da9c0728f03d802a1f5
Preethi <preethi.10bala@gmail.com>
2022-09-09 14:32:33 +0530

    Data preprocessing completed

--- main.py
+++ main.py
@@ -2,56 +2,65 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
-input_file=pd.read_csv("Dataset/Dataset.csv",low_memory=False,nrows=20000)
+input_file=pd.read_csv("Dataset/Dataset.csv",low_memory=True,nrows=30000)
 
 print(input_file.info()) #gives information about the file
 
+nul = input_file.isnull().mean().sort_values()
+nul = nul[nul>0.1]
 
-# Checking for null values before preprocessing
-sns.set(rc={'figure.figsize':(10,10)})
-sns.set_style('whitegrid')
-sns.heatmap(input_file.isnull())
-plt.title('Null values visual plot',fontdict={'fontsize': 10})
-plt.legend(input_file.isnull())
-plt.show()
+nul_col = nul.sort_values(ascending = False).index
+data = input_file.drop(nul_col, axis = 1)
+data = data.dropna(axis = 0).reset_index(drop = True)
+print(data.info())
 
-input_missing=pd.DataFrame({'column':input_file.columns, 'percent_missing':input_file.isnull().sum()*100/len(input_file)})
-input_file.drop(input_missing[input_missing['percent_missing']>10].index,axis=1,inplace=True)
-print(input_file.isnull().sum())
-input_file.drop(['id'],axis=1,inplace=True)
-print(input_file.info())
-print(input_file.loan_status.value_counts())
+obj_categorical = [feature for feature in data.columns if data[feature].dtype == "O"]
+print(obj_categorical)
 
-# Checking for null values after null value fields
-sns.heatmap(input_file.isnull())
-plt.title('Null values visual plot',fontdict={'fontsize': 10})
-plt.legend(input_file.isnull())
-plt.show()
+for value in obj_categorical:
+    print(value)
+    print(data[value].nunique())
 
-#drop the rows with loan status as current
-input_file.drop(input_file.index[input_file['loan_status'] == 'Current'], inplace=True)
+data.drop(['id', 'url'], axis = 1, inplace = True)
+data.drop(['sub_grade', 'emp_title'], axis = 1, inplace = True)
+data.drop(['title', 'zip_code'], axis = 1, inplace = True)
 
-input_file.loc[input_file['emp_length'].isnull(), 'emp_length'] = '10+ years'
+date_col = ['issue_d', 'earliest_cr_line', 'last_pymnt_d', 'last_credit_pull_d']
+
+for value in date_col:
+    data[value + '_month'] = data[value].apply(lambda x : x[0:3])
+    data[value + '_year'] = data[value].apply(lambda x : x[-4: ])
+
+data.drop(date_col, axis = 1, inplace = True)
+
+month_order = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+for column in date_col:
+    data[column + '_month'] = data[column + '_month'].apply(lambda x : month_order.index(x))
+print(data.info())
+
+for column in data.columns:
+    try:
+        data[column] = data[column].astype(float)
+        
+    except:
+        pass
+print(data.info())
+
+replace_status = {"Fully Paid":"Paid",
+             "Current": "Paid",
+             "Charged Off": "Default",
+              "Does not meet the credit policy. Status:Charged Off":"Default",
+              "Does not meet the credit policy. Status:Charged Off":"Default",
+              "Does not meet the credit policy. Status:Fully Paid":"Paid",
+              "Late (31-120 days)":"Late",
+              "Late (16-30 days)":"Late",
+              "In Grace Period":"Late",
+              "Default":"Default"
+             }
+data["loan_status"] = data["loan_status"].replace(replace_status)
+data = data[ (data["loan_status"]== "Paid") | (data["loan_status"]== "Default")]
 
 
-#transforming loan status from categorical to numeric
-input_file['loan_status_numeric']=input_file['loan_status'].map({'Charged Off':1,'Fully Paid':0,'Late (31-120 days)':0,'Late (16-30 days)':0,'In Grace Period':1})
-
-#feature extraction
-columns=['loan_amnt','term','int_rate','installment','grade','emp_length','home_ownership','annual_inc',
-        'verification_status','loan_status_numeric','purpose','addr_state','dti','delinq_2yrs','fico_range_low',
-        'inq_last_6mths','open_acc','pub_rec','revol_bal','revol_util','total_acc']
-
-input_file=input_file[columns]
-
-#employee length count
-print(input_file.emp_length.value_counts())
-
-#Checking for null values after feature extraction
-sns.heatmap(input_file.isnull())
-plt.title('Null values visual plot',fontdict={'fontsize': 10})
-plt.legend(input_file.isnull())
-plt.show()
-
-print(input_file['loan_status_numeric'].value_counts())
+print(data.info())
